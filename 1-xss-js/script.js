3ae96e0f5b33142839184635e0e1fdce51c39139
Joakim <wahlstrom.joakim@gmail.com>
2022-10-26 14:25:40 +0200

    lektion 3

--- 1-xss-js/script.js
+++ 1-xss-js/script.js
@@ -2,11 +2,11 @@ const form = document.querySelector('#messageForm');
 const output = document.querySelector('#output');
 
 const messages = [
-  {
-    headline: 'En rubrik', 
-    body: 'meddelande', 
-    imageUrl: 'https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80'
-  },
+  // {
+  //   headline: 'En rubrik', 
+  //   body: 'meddelande', 
+  //   imageUrl: 'https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80'
+  // },
   {
     headline: 'Message Headline', 
     body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque itaque illum quam! Error deserunt quaerat quam perspiciatis debitis consectetur quibusdam maxime quasi iusto, nam doloribus asperiores, soluta laborum, voluptates vitae?', 
@@ -14,21 +14,62 @@ const messages = [
   },
 ];
 
+const htmlEncode = (dirty) => {
+  const encoded = dirty.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/&/g, '&amp;').replace(/'/g, '&apos;').replace(/onerror/g, '')
+  console.log(dirty);
+  console.log(encoded);
+  return encoded;
+}
+
+const createMessageElement = message => {
+
+  const encodedBody = htmlEncode(message.body)
+
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('message');
+
+  const messageHeader = document.createElement('header');
+  messageHeader.classList.add('message-header');
+
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('img-container');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', message.imageUrl);
+
+  const headline = document.createElement('h2');
+  headline.innerText = message.headline;
+
+  const messageBody = document.createElement('div');
+  messageBody.classList.add('message-body');
+  messageBody.innerHTML = encodedBody;
+
+  imgContainer.appendChild(img);
+  messageHeader.appendChild(imgContainer);
+  messageHeader.appendChild(headline);
+  messageDiv.appendChild(messageHeader);
+  messageDiv.appendChild(messageBody);
+
+  return messageDiv;
+}
+
 const renderMessage = (message) => {
 
-  output.insertAdjacentHTML('beforeend', `
-    <div class="message">
-      <header class="message-header">
-        <div class="img-container">
-          <img src="${message.imageUrl}" alt="">
-        </div>
-        <h2>${message.headline}</h2>
-      </header>
-      <div class="message-body">
-        ${message.body}
-      </div>
-    </div>
-  `)
+  // output.insertAdjacentHTML('beforeend', `
+  //   <div class="message">
+  //     <header class="message-header">
+  //       <div class="img-container">
+  //         <img src="${message.imageUrl}" alt="">
+  //       </div>
+  //       <h2>${message.headline}</h2>
+  //     </header>
+  //     <div class="message-body">
+  //       ${message.body}
+  //     </div>
+  //   </div>
+  // `)
+
+  output.appendChild(createMessageElement(message));
 
 }
 
@@ -36,19 +77,20 @@ const renderMessages = () => {
   output.innerHTML = ''
 
   for(const message of messages) {
-    output.insertAdjacentHTML('beforeend', `
-    <div class="message">
-      <header class="message-header">
-        <div class="img-container">
-          <img src="${message.imageUrl}" alt="">
-        </div>
-        <h2>${message.headline}</h2>
-      </header>
-      <div class="message-body">
-        ${message.body}
-      </div>
-    </div>
-  `)
+  //   output.insertAdjacentHTML('beforeend', `
+  //   <div class="message">
+  //     <header class="message-header">
+  //       <div class="img-container">
+  //         <img src="${message.imageUrl}" alt="">
+  //       </div>
+  //       <h2>${message.headline}</h2>
+  //     </header>
+  //     <div class="message-body">
+  //       ${message.body}
+  //     </div>
+  //   </div>
+  // `)
+  output.appendChild(createMessageElement(message));
   }
 }
 
